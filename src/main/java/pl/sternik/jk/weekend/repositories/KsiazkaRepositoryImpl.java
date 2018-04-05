7a52ae50fe0fd6bf813fc9c97a47e8fd6f3b59b1
JanKocon <jan.kocon@edu.uekat.pl>
2018-04-05 14:15:05 +0200

    Add some books

--- src/main/java/pl/sternik/jk/weekend/repositories/KsiazkaRepositoryImpl.java
+++ src/main/java/pl/sternik/jk/weekend/repositories/KsiazkaRepositoryImpl.java
@@ -20,12 +20,15 @@ private List<Ksiazka> ksiazki = new ArrayList<Ksiazka>(){
         //Long numerKatalogowy, String tytul, String opis, double cena, String autor, String gatunek, Date dataWydania, Status status
 
         add(Ksiazka.produceKsiazka(1L,"W pustyni i w puszczy","Książka jak książka",100d,"Henryk Sienkiewicz","Literatura młodzieżowa",KsiazkaUtils.getDateWithYearAndMonthForDay(1911,1,2), Stan.UZYWANA));
-        add(Ksiazka.produceKsiazka(2L,"Gra o tron","Książka jak książka",229.99,"George R.R. Martin","Fantasy",KsiazkaUtils.getDateWithYearAndMonthForDay(1996,8,1),Stan.NOWA));
-        add(Ksiazka.produceKsiazka(3L,"Hobbit","Książka jak książka",45.50,"John Ronald Reuel Tolkien","Fantasy",KsiazkaUtils.getDateWithYearAndMonthForDay(1937,9,21),Stan.UZYWANA));
+        add(Ksiazka.produceKsiazka(2L,"Gra o tron","Książka jak książka",229.99,"George R.R. Martin","Fantasy",KsiazkaUtils.getDateWithYearAndMonthForDay(1996,8,1),Stan.DO_SPRZEDANIA));
+        add(Ksiazka.produceKsiazka(3L,"Hobbit","Książka jak książka",45.50,"John Ronald Reuel Tolkien","Fantasy",KsiazkaUtils.getDateWithYearAndMonthForDay(1937,9,21),Stan.DO_SPRZEDANIA));
         add(Ksiazka.produceKsiazka(4L,"Lalka","Książka jak książka",25d,"Bolesław Prus","Powieść społeczna",KsiazkaUtils.getDateWithYearAndMonthForDay(1890,5,2),Stan.UZYWANA));
         add(Ksiazka.produceKsiazka(5L,"Thinking in java","Książka jak książka",119.99,"Bruce Eckel","Poradnik do javy",KsiazkaUtils.getDateWithYearAndMonthForDay(2017,3,3),Stan.NOWA));
         add(Ksiazka.produceKsiazka(6L,"Arrow","Książka jak książka",69.99,"Bruce Lee","Fikcja",KsiazkaUtils.getDateWithYearAndMonthForDay(2012,5,7),Stan.NOWA));
-        add(Ksiazka.produceKsiazka(7L,"Egzekutor","Książka jak książka",74.99,"Chris Carter","Dreszczowiec",KsiazkaUtils.getDateWithYearAndMonthForDay(2013,5,4),Stan.DUBLET));
+        add(Ksiazka.produceKsiazka(7L,"Egzekutor","Książka jak książka",74.99,"Chris Carter","Dreszczowiec",KsiazkaUtils.getDateWithYearAndMonthForDay(2013,5,4),Stan.NOWA));
+        add(Ksiazka.produceKsiazka(8L,"Umysł początkującego","Książka",119.99,"Shunryu Suzuki","Filozofia zen",KsiazkaUtils.getDateWithYearAndMonthForDay(2010,8,12),Stan.DO_SPRZEDANIA));
+        add(Ksiazka.produceKsiazka(9L,"Arrow","Książka jak książka",69.99,"Bruce Lee","Fikcja",KsiazkaUtils.getDateWithYearAndMonthForDay(2012,5,7),Stan.DUBLET));
+        add(Ksiazka.produceKsiazka(10L,"Egzekutor","Książka jak książka",74.99,"Chris Carter","Dreszczowiec",KsiazkaUtils.getDateWithYearAndMonthForDay(2013,5,4),Stan.DUBLET));
     }
 
 };
